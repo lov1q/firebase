215afc5d5a05f57cdd00b05911dddd828f7447a7
lov1q <n.st1mer@mail.ru>
2024-11-22 03:04:54 +0300

    read data

--- app/src/main/java/com/example/firebase/MainActivity.kt
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firebase
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val db = Firebase.firestore
+        val textViewData: TextView = findViewById(R.id.textview)
 
         // Create a new user with a first, middle, and last name
         val user = hashMapOf(
@@ -24,13 +26,33 @@ class MainActivity : AppCompatActivity() {
         )
 
         // Add a new document with a generated ID
-        db.collection("users")
+        /*db.collection("users")
             .add(user)
             .addOnSuccessListener { documentReference ->
                 Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
             }
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
+            }*/
+
+        db.collection("Student")
+            .get()
+            .addOnSuccessListener { result ->
+                val dataBuilder = StringBuilder() // Для сборки данных в строку
+                for (document in result) {
+                    // Добавляем данные в StringBuilder
+                    dataBuilder.append("${document.id} => ${document.data}\n")
+                }
+                // Устанавливаем данные в TextView
+                textViewData.text = if (dataBuilder.isNotEmpty()) {
+                    dataBuilder.toString()
+                } else {
+                    "Нет данных в коллекции Students"
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("MainActivity", "Error getting documents.", exception)
+                textViewData.text = "Ошибка загрузки данных."
             }
     }
 }
